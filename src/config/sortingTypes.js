2fc76684bd9d499b1889e27e8ab3792c26e04680
Aikin Kakim <aikinkaklm@gmail.com>
2025-09-04 14:30:46 +0500

    config: sortingTypes

---
+++ src/config/sortingTypes.js
@@ -0,0 +1,5 @@
+export const sortingTypes = [
+  { name: 'популярности', sort: 'rating', order: 'desc' },
+  { name: 'цене', sort: 'price', order: 'desc' },
+  { name: 'алфавиту', sort: 'title', order: 'asc' },
+]
